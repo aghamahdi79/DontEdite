af29e685adc357a95725e864a661f754f972bdb1
Cyber <www.mohammad.blogfa111@gmail.com>
2016-09-14 12:46:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ob_start();
-define('API_KEY','token');
+define('API_KEY','285187183:AAEJPHY_EcBfcJUvzFCoaSihGJ4X9elZx4I');
 $admin = "159887854";
 function bot($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
